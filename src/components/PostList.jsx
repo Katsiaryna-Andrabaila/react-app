98c2790cacd3b4c85b5058b848afb17709025207
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-09-17 16:26:09 +0200

    refactor: move condition to post list component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,14 +2,20 @@
 import PostItem from "./PostItem";
 
 const PostList = ({posts, remove}) => {
+  if (!posts.length) {
     return (
-        <>
-        <h1 style={{ textAlign: "center" }}>Post list</h1>
+      <h1 style={{ textAlign: "center" }}>Posts not found!</h1>
+    )
+  }
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>Post list</h1>
         {posts.map((post, i) => (
           <PostItem remove={remove} number={i + 1} key={post.id} {...post} />
         ))}
-      </>
-    );
+    </>
+  );
   }
   
   export default PostList;
